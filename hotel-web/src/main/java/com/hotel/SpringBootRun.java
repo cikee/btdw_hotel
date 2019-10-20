8c1ab701cd5c636338d5c2b2ef907ae6b9f6064d
lzx1325659983 <110@2b3k.com>
2019-10-20 16:16:20 +0800

    lzx

--- hotel-web/src/main/java/com/hotel/SpringBootRun.java
+++ hotel-web/src/main/java/com/hotel/SpringBootRun.java
@@ -10,7 +10,7 @@ import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 public class SpringBootRun {
 	
 	public static void main(String[] args) {
-
+ 
 		SpringApplication.run(SpringBootRun.class, args);
 	}
 }
